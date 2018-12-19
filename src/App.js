df7f4241a9f18adca08166a557b88a5a3febd835
Fabrice Million <fabrice@funkadelik.com>
2018-12-19 13:58:17 +1100

    initial commit

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ class App extends Component {
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <p>
-            Edit <code>src/App.js</code> and save to reload.
+            Edit <span>src/App.js</span> and save to reload.
           </p>
           <a
             className="App-link"
@@ -24,5 +24,4 @@ class App extends Component {
     );
   }
 }
-
 export default App;
